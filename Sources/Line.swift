b543415891e5e64d67b8c328835d9c2f5d17830e
sbstjn <mail@sbstjn.com>
2016-06-27 19:10:25 +0200

    feat(Line): Add more features to Line object.

--- Sources/Line.swift
+++ Sources/Line.swift
@@ -7,25 +7,74 @@ import Foundation
 #endif
 
 public class Line: CustomStringConvertible {
-    
+
     var a: Point
     var b: Point
-    
+
     public init(a: Point, b: Point) {
         self.a = a
         self.b = b
     }
-    
-    public var slope: Double {
-        return (a.y - b.y) / (a.x - b.x)
-    }
-    
-    public func contains(p: Point) -> Bool {
-        return p.y == self.slope * (p.x - a.x) + a.y
-    }
-        
+
     public var description: String {
         return "Line (a:\(self.a), b:\(self.b))"
     }
-    
+
+    public var slope: Double {
+        return (a.y - b.y) / (a.x - b.x)
+    }
+
+    public func calculate(x: Double) -> Double {
+        return slope * (x - a.x) + a.y;
+    }
+
+    public func contains(p: Point) -> Bool {
+        return p.y == calculate(x: p.x)
+    }
+
+    public func isAbove(p: Point) -> Bool {
+        return p.y > calculate(x: p.x)
+    }
+
+    public func isBelow(p: Point) -> Bool {
+        return p.y < calculate(x: p.x)
+    }
+
+    public func isAbove(l: Line) -> Bool {
+        return !crosses(l: l) && calculate(x: a.x) > l.calculate(x: a.x)
+    }
+
+    public func isBelow(l: Line) -> Bool {
+        return !crosses(l: l) && calculate(x: a.x) < l.calculate(x: a.x)
+    }
+
+    public func isAbove(l: Line, x: Double) -> Bool {
+        return calculate(x: x) > l.calculate(x: x)
+    }
+
+    public func isBelow(l: Line, x: Double) -> Bool {
+        return calculate(x: x) < l.calculate(x: x)
+    }
+
+    public func isEqual(l: Line) -> Bool {
+        return contains(p: l.a) && l.slope == slope
+    }
+
+    public func parallel(l: Line) -> Bool {
+        return !crosses(l: l) && !isEqual(l: l)
+    }
+
+    public func crosses(l: Line) -> Bool {
+        return slope != l.slope
+    }
+
+    public func crossesAt(l: Line) -> Point {
+        let tmp1 = slope - l.slope
+        let tmp2 = l.a.y + a.y
+
+        let pos = tmp2 / tmp1
+
+        return Point(x: pos, y: calculate(x: pos))
+    }
+
 }
--- Tests/Boilerplate/LineTests.swift
+++ Tests/Boilerplate/LineTests.swift
@@ -20,20 +20,158 @@ class LineTests: XCTestCase {
 
         XCTAssertEqual(l.description, "Line (a:Point (x:1.0, y:2.0), b:Point (x:4.0, y:4.0))")
     }
-    
+
     func testContains() {
         let a = Point(x: 1, y: 2)
         let b = Point(x: 4, y: 4)
         let c = Point(x: 5, y: 5)
-        
+
         let l = Line(a: a, b: b)
-        
+
         XCTAssertTrue(l.contains(p: a))
         XCTAssertTrue(l.contains(p: b))
-        
+
         XCTAssertFalse(l.contains(p: c))
     }
 
+    func testIsAbovePoint() {
+        let a = Point(x: 1, y: 2)
+        let b = Point(x: 4, y: 4)
+
+        let l = Line(a: a, b: b)
+
+        XCTAssertTrue(l.isAbove(p: Point(x: 1, y: 3)))
+        XCTAssertFalse(l.isAbove(p: Point(x: 1, y: 1)))
+        XCTAssertFalse(l.isAbove(p: Point(x: 1, y: 2)))
+    }
+
+    func testIsBelowPoint() {
+        let a = Point(x: 1, y: 2)
+        let b = Point(x: 4, y: 4)
+
+        let l = Line(a: a, b: b)
+
+        XCTAssertFalse(l.isBelow(p: Point(x: 1, y: 3)))
+        XCTAssertFalse(l.isBelow(p: Point(x: 1, y: 2)))
+        XCTAssertTrue(l.isBelow(p: Point(x: 1, y: 1)))
+    }
+
+    func testIsBelowLine() {
+        let a = Point(x: 1, y: 2)
+        let b = Point(x: 2, y: 2)
+
+        let c = Point(x: 1, y: 4)
+        let d = Point(x: 2, y: 4)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l1.isBelow(l: l2))
+        XCTAssertFalse(l2.isBelow(l: l1))
+        XCTAssertFalse(l2.isBelow(l: l1))
+    }
+
+    func testIsAboveLine() {
+        let a = Point(x: 1, y: 2)
+        let b = Point(x: 2, y: 2)
+
+        let c = Point(x: 1, y: 4)
+        let d = Point(x: 2, y: 4)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l2.isAbove(l: l1))
+        XCTAssertFalse(l1.isAbove(l: l2))
+        XCTAssertFalse(l1.isAbove(l: l2))
+    }
+
+    func testIsBelowLineAtPoint() {
+        let a = Point(x: 0, y: 0)
+        let b = Point(x: 1, y: 1)
+
+        let c = Point(x: 0, y: 0)
+        let d = Point(x: -1, y: 1)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l1.isBelow(l: l2, x: -1))
+        XCTAssertTrue(l2.isBelow(l: l1, x:  1))
+
+        XCTAssertFalse(l2.isBelow(l: l1, x: 0))
+        XCTAssertFalse(l1.isBelow(l: l2, x: 0))
+        XCTAssertFalse(l1.isBelow(l: l1, x: 0))
+    }
+
+    func testIsAboveLineAtPoint() {
+        let a = Point(x: 0, y: 0)
+        let b = Point(x: 1, y: 1)
+
+        let c = Point(x: 0, y: 0)
+        let d = Point(x: -1, y: 1)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l2.isAbove(l: l1, x: -1))
+        XCTAssertTrue(l1.isAbove(l: l2, x:  1))
+
+        XCTAssertFalse(l2.isAbove(l: l1, x: 0))
+        XCTAssertFalse(l1.isAbove(l: l2, x: 0))
+        XCTAssertFalse(l1.isAbove(l: l1, x: 0))
+    }
+
+    func testIsEqual() {
+        let a = Point(x: 1, y: 2)
+        let b = Point(x: 4, y: 4)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: b, b: a)
+
+        let c = Point(x: 8, y: l1.calculate(x: 8))
+        let d = Point(x: 9, y: 1)
+
+        let l3 = Line(a: a, b: c)
+        let l4 = Line(a: c, b: d)
+
+        XCTAssertTrue(l1.isEqual(l: l2))
+        XCTAssertTrue(l1.isEqual(l: l3))
+
+        XCTAssertFalse(l1.isEqual(l: l4))
+    }
+
+    func testCrosses() {
+        let a = Point(x: 0, y: 0)
+        let b = Point(x: 1, y: 1)
+
+        let c = Point(x: 0, y: 0)
+        let d = Point(x: -1, y: 1)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l1.crosses(l: l2))
+    }
+
+    func testCrossesAt() {
+        let a = Point(x: 0, y: 0)
+        let b = Point(x: 1, y: 1)
+
+        let c = Point(x: 0, y: 0)
+        let d = Point(x: -1, y: 1)
+
+        let l1 = Line(a: a, b: b)
+        let l2 = Line(a: c, b: d)
+
+        XCTAssertTrue(l1.crosses(l: l2))
+
+        let expected = Point(x: 0, y: 0)
+        let crossing = l1.crossesAt(l: l2)
+
+        XCTAssertEqual(crossing.x, expected.x)
+        XCTAssertEqual(crossing.y, expected.y)
+    }
 }
 
 
@@ -43,6 +181,15 @@ class LineTests: XCTestCase {
             return [
                 ("testDescription", testDescription),
                 ("testContains", testContains),
+                ("testIsAbovePoint", testIsAbovePoint),
+                ("testIsAboveLine", testIsAboveLine),
+                ("testIsAboveLineAtPoint", testIsAboveLineAtPoint),
+                ("testIsBelowPoint", testIsBelowPoint),
+                ("testIsBelowLine", testIsBelowLine),
+                ("testIsBelowLineAtPoint", testIsBelowLineAtPoint),
+                ("testIsEqual", testIsEqual),
+                ("testCorsses", testCrosses),
+                ("testCorssesAt", testCrossesAt),
             ]
         }
     }
